b8bc021bb46388dd6fb8dc5909ad62320122bb6e
Vincent Weevers <mail@vincentweevers.nl>
2022-02-19 13:22:18 +0100

    Document default value of `highWaterMark`

--- index.d.ts
+++ index.d.ts
@@ -56,6 +56,8 @@ export interface ReadStreamOptions {
   /**
    * The maximum number of items to buffer internally before ceasing to read further
    * items.
+   *
+   * @defaultValue `1000`
    */
   highWaterMark?: number | undefined
 }
